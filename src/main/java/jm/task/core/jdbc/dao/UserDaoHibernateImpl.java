e8a4945ac32cae2728e8786d4f26d27bb97fbae8
Myzery1 <myzery38@gmail.com>
2022-06-20 00:35:06 +0700

    поля private, убрана транзакционная логика, добавлен rollback

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -11,7 +11,7 @@ import static jm.task.core.jdbc.util.Util.getSessionFactory;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-    Session session = getSessionFactory().openSession();
+    Session session;
 
     public UserDaoHibernateImpl() {
 
